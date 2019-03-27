68e35832ec3e9829d7c47875e8deccd55d78fc4a
Juan Marin Otero <juan.marin.otero@gmail.com>
2019-03-27 16:11:37 -0400

    Update Employee generator

--- src/test/java/serializer/EmployeeGenerator.java
+++ src/test/java/serializer/EmployeeGenerator.java
@@ -5,6 +5,16 @@ import com.pholser.junit.quickcheck.generator.Generator;
 import com.pholser.junit.quickcheck.random.SourceOfRandomness;
 
 public class EmployeeGenerator extends Generator<Employee> {
+
+    private static final String[] FIST_NAMES = new String[]{
+            "Joe", "Andy", "Vivian", "Amy", "Rodolfo", "Mary"
+    };
+
+
+    private static final String[] LAST_NAMES = new String[]{
+            "Garcia", "Smith", "Doe", "Simpson", "Snow"
+    };
+
     public EmployeeGenerator() {
         super(Employee.class);
     }
@@ -12,8 +22,8 @@ public class EmployeeGenerator extends Generator<Employee> {
     @Override
     public Employee generate(SourceOfRandomness random, GenerationStatus status) {
 
-        String firstName = "";
-        String lastName = "";
+        String firstName = FIST_NAMES[random.nextInt(6)];
+        String lastName = LAST_NAMES[random.nextInt(5)];
         int age = new NonNegativeInts()
                 .filter(x -> x < 100)
                 .generate(random, status);
